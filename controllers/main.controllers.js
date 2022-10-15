fdaf6ed904741eda8d8df7144e44a3948648c357
Vighnesh Manjrekar <vighneshmanjrekar4193@gmail.com>
2022-10-15 23:45:14 +0530

    Fix controller comments

--- controllers/main.controllers.js
+++ controllers/main.controllers.js
@@ -2,55 +2,55 @@ const Client = require("../models/Client.model");
 const Project = require("../models/Project.model");
 
 // @desc    Get all projects and clients
-// @route   GET /
+// @route   GET api/
 exports.getIndex = async (req, res, next) => {
   // Fetch all projects and clients
 };
 
 // @desc    Create project
-// @route   POST /project
+// @route   POST api/project
 exports.postProject = async (req, res, next) => {
   // Create project
 };
 
 // @desc    Create client
-// @route   POST /client
+// @route   POST api/client
 exports.postClient = async (req, res, next) => {
   // Create client
 };
 
 // @desc    Get single project
-// @route   GET /project/:id
+// @route   GET api/project/:id
 exports.getProject = async (req, res, next) => {
   // Fetch single project
 };
 
 // @desc    Get single client
-// @route   GET /client/:id
+// @route   GET api/client/:id
 exports.getClient = async (req, res, next) => {
   // Fetch single client
 };
 
 // @desc    Delete project
-// @route   DELETE /project/:id
+// @route   DELETE api/project/:id
 exports.deleteProject = async (req, res, next) => {
   // Delete project with req.params.id
 };
 
 // @desc    Delete client
-// @route   DELETE /client/:id
+// @route   DELETE api/client/:id
 exports.deleteClient = async (req, res, next) => {
   // Delete client with req.params.id
 };
 
 // @desc    Update project
-// @route   PUT /project/:id
+// @route   PUT api/project/:id
 exports.updateProject = async (req, res, next) => {
   // Update project with req.params.id
 };
 
 // @desc    Update client
-// @route   PUT /client/:id
+// @route   PUT api/client/:id
 exports.updateClient = async (req, res, next) => {
   // Update client with req.params.id
 };
