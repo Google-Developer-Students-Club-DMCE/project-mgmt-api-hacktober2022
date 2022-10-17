dfb7b798de7d4309b518663d178c5f5a00b37a6d
Rohan Yadav <yrohan740@gmail.com>
2022-10-17 13:30:17 +0530

    Feature: Include PUT project controller

--- controllers/main.controllers.js
+++ controllers/main.controllers.js
@@ -47,6 +47,27 @@ exports.deleteClient = async (req, res, next) => {
 // @route   PUT api/project/:id
 exports.updateProject = async (req, res, next) => {
   // Update project with req.params.id
+
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    return res
+      .status(400)
+      .json({ success: false, error: `Invalid Id ${req.params.id}` });
+  }
+
+  let project = await Project.findById(req.params.id);
+
+  if (!project) {
+    return res
+      .status(404)
+      .json({ success: false, error: `No project with id ${req.params.id}` });
+  }
+
+  project = await Project.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({ success: true, project });
 };
 
 // @desc    Update client
